58f68e10ea4407c85dc45ebed8a19ab5207a0d84
aditigandhi2701-hue <aditigandhi2701@gmail.com>
2026-07-02 16:31:53 +0530

    Initial commit

--- maths.cpp
+++ maths.cpp
@@ -29,3 +29,18 @@ public:
         return rev;
     }
 };
+class Solution {
+public:
+    bool palindromeNumber(int n) {
+        int original = n;
+        int rev = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+
+        return original == rev;
+    }
+};
