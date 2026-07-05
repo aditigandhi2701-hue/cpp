5e614c5a296a7c83ac26f07c88d2239bbf871740
aditigandhi2701-hue <aditigandhi2701@gmail.com>
2026-07-05 13:54:28 +0530

    Initial commit

--- recursion.cpp
+++ recursion.cpp
@@ -9,3 +9,18 @@ class Solution {
         cout<<n<<endl;
     }
 };
+class Solution {
+public:
+    void printNos(int n) {
+
+        // Base case
+        if (n == 0)
+            return;
+
+        // Print first
+        cout << n << endl;
+
+        // Recursive call
+        printNos(n - 1);
+    }
+};
