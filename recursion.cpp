849c60451921f68e0fbe7881a5f6a0aa792690ab
aditigandhi2701-hue <aditigandhi2701@gmail.com>
2026-07-05 14:56:27 +0530

    Initial commit

--- recursion.cpp
+++ recursion.cpp
@@ -67,3 +67,18 @@ public:
         return isPalindrome(s, left + 1, right - 1);
     }
 };
+class Solution {
+public:
+    int fibonacci(int n) {
+
+        // Base cases
+        if (n == 0)
+            return 0;
+
+        if (n == 1)
+            return 1;
+
+        // Recursive case
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+};
