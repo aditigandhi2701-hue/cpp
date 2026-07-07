86e84fc613add4a1f41a934befce25bf697ffd1c
aditigandhi2701-hue <aditigandhi2701@gmail.com>
2026-07-07 17:51:23 +0530

    Initial commit

--- sorting_2.cpp
+++ sorting_2.cpp
@@ -38,3 +38,25 @@ public:
         return nums;
     }
 };
+class Solution {
+public:
+    vector<int> insertionSort(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int i = 1; i < n; i++) {
+            int key = nums[i];
+            int j = i - 1;
+
+            // Shift elements greater than key
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+
+            // Place key at the correct position
+            nums[j + 1] = key;
+        }
+
+        return nums;
+    }
+};
