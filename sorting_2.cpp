268cbe2b6fdef981437457bd25830ca9fd187d6c
aditigandhi2701-hue <aditigandhi2701@gmail.com>
2026-07-08 12:43:34 +0530

    Initial commit

--- sorting_2.cpp
+++ sorting_2.cpp
@@ -110,3 +110,62 @@ public:
         return nums;
     }
 };
+class Solution {
+public:
+    void merge(vector<int>& nums, int low, int mid, int high) {
+        vector<int> temp;
+
+        int left = low;
+        int right = mid + 1;
+
+        // Merge the two sorted halves
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                temp.push_back(nums[left]);
+                left++;
+            } else {
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+
+        // Remaining elements of left half
+        while (left <= mid) {
+            temp.push_back(nums[left]);
+            left++;
+        }
+
+        // Remaining elements of right half
+        while (right <= high) {
+            temp.push_back(nums[right]);
+            right++;
+        }
+
+        // Copy back to original array
+        for (int i = low; i <= high; i++) {
+            nums[i] = temp[i - low];
+        }
+    }
+
+    void mergeSort(vector<int>& nums, int low, int high) {
+        // Base case
+        if (low >= high)
+            return;
+
+        int mid = low + (high - low) / 2;
+
+        // Sort left half
+        mergeSort(nums, low, mid);
+
+        // Sort right half
+        mergeSort(nums, mid + 1, high);
+
+        // Merge both halves
+        merge(nums, low, mid, high);
+    }
+
+    vector<int> mergeSort(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+};
