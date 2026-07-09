7b1a8e0fbcdf1398896cda2898e33c3526793fe4
aditigandhi2701-hue <aditigandhi2701@gmail.com>
2026-07-09 13:15:03 +0530

    Initial commit

--- array_eas.cpp
+++ array_eas.cpp
@@ -35,3 +35,14 @@ public:
         return -1;             // Target not found
     }
 };
+class Solution {
+public:
+    bool isSorted(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
